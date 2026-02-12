8fec5a2709482c90ae69825db7a819f62dad7a1c
Kunal200317 <kunal200317@gmail.com>
2026-02-12 13:20:46 +0530

    problem

--- src/app/DB/cunnectDb.js
+++ src/app/DB/cunnectDb.js
@@ -1,16 +1,17 @@
-import mongoose from'mongoose';
+import mongoose from 'mongoose';
+
 
 const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-      });
-      console.log(`MongoDB Connected: {conn.connection.host}`);
-    } catch (error) {
-      console.error(error.message);
-      process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(`mongodb+srv://get-me-a-chai:kunal123@cluster0.vgojcu5.mongodb.net/food-ordaring?retryWrites=true&w=majority`, {
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected: {conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
   }
+}
 
-export default connectDB; 
+export default connectDB;
 
